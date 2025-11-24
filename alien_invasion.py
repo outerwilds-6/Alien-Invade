4f6bf02dca048f92e8e810bf5c6a123ed8f68901
outerwilds-6 <null>
2025-11-25 01:17:24 +0800

    bug fixes on saving highscore

--- alien_invasion.py
+++ alien_invasion.py
@@ -124,6 +124,7 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
+            self._store_highscore()
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
